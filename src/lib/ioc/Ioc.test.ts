ca520f5ead1575a6d949aec86a35c84a9ebe0285
Noirr <zaczkiewicz.radoslaw@gmail.com>
2022-06-10 23:00:34 +0100

    test(ioc): change variable keys in ioc container

--- src/lib/ioc/Ioc.test.ts
+++ src/lib/ioc/Ioc.test.ts
@@ -3,8 +3,8 @@ import IocContainer from './index';
 const IOC_TOKEN_KEY = 'StoreAccessToken';
 const IOC_USERNAME_KEY = 'StoreUsername';
 const IOC_PASSWORD_KEY = 'StorePassword';
-const ICO_REPOSITORY_KEY = 'StoreRepository';
-const ACCESS_TOKEN_GENERATOR_KEY = 'AccessTokenGenerator';
+const IOC_REPOSITORY_KEY = 'StoreRepository';
+const IOC_TOKEN_GENERATOR_KEY = 'AccessTokenGenerator';
 
 const STORE_USERNAME = 'example-username';
 const STORE_PASSWORD = 'example-password';
@@ -32,11 +32,11 @@ class StoreRepository {
 describe('commonElements', () => {
   const container = new IocContainer();
 
-  container.register(IOC_TOKEN_KEY, (c) => c.get(ACCESS_TOKEN_GENERATOR_KEY)(c.get(IOC_USERNAME_KEY), c.get(IOC_PASSWORD_KEY)));
+  container.register(IOC_TOKEN_KEY, (c: IocContainer) => c.get(IOC_TOKEN_GENERATOR_KEY)(c.get(IOC_USERNAME_KEY), c.get(IOC_PASSWORD_KEY)));
   container.register(IOC_USERNAME_KEY, () => STORE_USERNAME);
   container.register(IOC_PASSWORD_KEY, () => STORE_PASSWORD);
-  container.register(ACCESS_TOKEN_GENERATOR_KEY, () => createStoreAccessToken);
-  container.register(ICO_REPOSITORY_KEY, (c) => new StoreRepository(c.get(IOC_TOKEN_KEY)));
+  container.register(IOC_TOKEN_GENERATOR_KEY, () => createStoreAccessToken);
+  container.register(IOC_REPOSITORY_KEY, (c: IocContainer) => new StoreRepository(c.get(IOC_TOKEN_KEY)));
 
   test('Check username', () => {
     const username = container.get(IOC_USERNAME_KEY)
@@ -44,7 +44,7 @@ describe('commonElements', () => {
   })
 
   test('Check generator function', () => {
-    const generator = container.get(ACCESS_TOKEN_GENERATOR_KEY)
+    const generator = container.get(IOC_TOKEN_GENERATOR_KEY)
     expect(generator).toEqual(createStoreAccessToken)
   })
 
@@ -54,19 +54,19 @@ describe('commonElements', () => {
   })
 
   test('Check store access token generator', () => {
-    const tokenGenerator = container.get(ACCESS_TOKEN_GENERATOR_KEY);
+    const tokenGenerator = container.get(IOC_TOKEN_GENERATOR_KEY);
     const exampleToken = tokenGenerator('a', 'b');
     expect(exampleToken).toEqual(`a_b`);
   })
 
   test('Check store access token from repository object', () => {
-    const storeRepository = container.get(ICO_REPOSITORY_KEY);
+    const storeRepository = container.get(IOC_REPOSITORY_KEY);
     const repositoryToken = storeRepository.getToken();
     expect(repositoryToken).toEqual(TOKEN_1);
   })
 
   test('Change token generator', () => {
-    container.register(ACCESS_TOKEN_GENERATOR_KEY, () => anotherCreateStoreAccessToken);
+    container.register(IOC_TOKEN_GENERATOR_KEY, () => anotherCreateStoreAccessToken);
     const newToken = container.get(IOC_TOKEN_KEY);
     expect(newToken).toEqual(TOKEN_2);
   })
