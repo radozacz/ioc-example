7d729ab25b6bc1cf963500c9566503314ac5ecf1
Noirr <zaczkiewicz.radoslaw@gmail.com>
2022-06-10 22:42:47 +0100

    feat(ioc): ioc container class

--- src/lib/ioc/Ioc.ts
+++ src/lib/ioc/Ioc.ts
@@ -1,5 +1,25 @@
-class Ioc {
+class IocContainer {
+  private saved = {};
 
+  /**
+   * save key
+   */
+  register = (name: string, cb: Function) => {
+    this.saved[name] = cb;
+  }
+
+  /**
+   * get saved key
+   */
+  get = (name: string) => {
+    if(this.saved.hasOwnProperty(name)){
+      const resolver = this.saved[name];
+
+      return resolver(this);
+    }
+
+    throw new Error(`IocContainer does not have saved key "${name}".`);
+  }
 }
 
-export default Ioc;
+export default IocContainer;
